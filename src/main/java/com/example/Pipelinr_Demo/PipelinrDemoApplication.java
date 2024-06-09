e1af97d0562b68828d07e3b861a0abc6d9b4bf16
Tuan Doan <doantuan247.hs@gmail.com>
2024-06-09 23:47:06 +0700

    update: config

---
+++ src/main/java/com/example/Pipelinr_Demo/PipelinrDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.Pipelinr_Demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PipelinrDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PipelinrDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Pipelinr_Demo/PipelinrDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Pipelinr_Demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PipelinrDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
